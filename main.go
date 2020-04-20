5e7444321717b78a8586c1f2fba692a5979c7b89
benka-me <newcratie@gmail.com>
2020-04-20 14:00:01 +0200

    bee.yaml indent

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/benka-me/laruche-oauth/server"
+	"github.com/benka-me/laruche/go-pkg/discover"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Commands = cli.Commands{
+		{
+			Name: "dev",
+			Action: func(context *cli.Context) error {
+				if len(os.Args) < 3 {
+					fmt.Println("usage: exec author/service-name")
+					os.Exit(0)
+				}
+				engine, err := discover.ParseEngine(os.Args[2], true)
+				if err != nil {
+					log.Fatal(err)
+				}
+				return server.Run(engine)
+			},
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if len(os.Args) < 2 {
+			fmt.Println("usage: exec author/service-name")
+			os.Exit(0)
+		}
+		engine, err := discover.ParseEngine(os.Args[1], false)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return server.Run(engine)
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
